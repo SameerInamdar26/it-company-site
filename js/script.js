63ee323271a87a24fa6cd975428641a60e2bbe31
Sam <saminamdar2310@gmail.com>
2025-06-23 23:42:52 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// 🍔 Toggle mobile nav
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".toggle-nav");
+  const navLinks = document.querySelector(".nav-links");
+
+  if (toggleButton && navLinks) {
+    toggleButton.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+  }
+});
+
